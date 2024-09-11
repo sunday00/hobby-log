f8e50e07b89dc5234e077136555d4d81cb2b0970
sunday00 <kind65505256@gmail.com>
2024-09-11 16:54:18 +0900

    sub images input update

--- src/main/java/net/grayfield/spb/hobbylog/domain/image/ImageController.java
+++ src/main/java/net/grayfield/spb/hobbylog/domain/image/ImageController.java
@@ -4,23 +4,32 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.grayfield.spb.hobbylog.domain.image.struct.AddSubImageInput;
 import net.grayfield.spb.hobbylog.domain.image.struct.ImageUsedAs;
+import net.grayfield.spb.hobbylog.domain.share.StaticHelper;
+import net.grayfield.spb.hobbylog.domain.share.struct.Category;
 import net.grayfield.spb.hobbylog.domain.share.struct.Result;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 
+import java.io.FileNotFoundException;
+import java.time.LocalDateTime;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class ImageController {
     private final ImageService imageService;
+    private final FileSystemService fileSystemService;
 
     @PreAuthorize("hasRole('ROLE_WRITER')")
     @MutationMapping
-    public Result addSubImage(@Argument AddSubImageInput addSubImageInput)  {
+    public Result addSubImage(@Argument AddSubImageInput addSubImageInput) throws FileNotFoundException {
+        LocalDateTime logAt = StaticHelper.generateLogAt(addSubImageInput.getLogAtStr());
+        String folder = this.fileSystemService.makeCategoryImageFolder(addSubImageInput.getCategory(), logAt);
+
         String path = this.imageService.storeSubImage(
-                addSubImageInput.getFolder(),
+                folder,
                 addSubImageInput.getUrl(),
                 addSubImageInput.getId(),
                 addSubImageInput.getSubId(),
--- src/main/java/net/grayfield/spb/hobbylog/domain/image/struct/AddSubImageInput.java
+++ src/main/java/net/grayfield/spb/hobbylog/domain/image/struct/AddSubImageInput.java
@@ -1,13 +1,15 @@
 package net.grayfield.spb.hobbylog.domain.image.struct;
 
 import lombok.Data;
+import net.grayfield.spb.hobbylog.domain.share.struct.Category;
 import org.springframework.lang.Nullable;
 
 @Data
 public class AddSubImageInput {
     private String id;
-    private String folder;
+    private Category category;
     private String url;
+    private String logAtStr;
     private int subId;
 
     @Nullable
